6eddabfeffc0572ff8810a3439e3ab2e3848d78b
leeyj-hy <dldydwo1230@hanyang.ac.kr>
2024-02-05 06:11:38 -0800

    structure added

---
+++ CMakeFiles/RMD_X_SERIES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RMD_X_SERIES.dir/main.cpp.o"
+  "RMD_X_SERIES"
+  "RMD_X_SERIES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RMD_X_SERIES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
